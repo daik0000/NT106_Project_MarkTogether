07d9362a7150a316e098b9499611ac38d054994e
nanad017 <daoan426@gmail.com>
2026-04-12 09:26:15 +0700

    Them LoginForm giao dien dang nhap

---
+++ MarkTogether.Client/LoginForm.Designer.cs
@@ -0,0 +1,193 @@
+namespace MarkTogether.Client
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblSubtitle = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lnkRegister = new System.Windows.Forms.LinkLabel();
+            this.lblError = new System.Windows.Forms.Label();
+            this.pnlMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pnlMain.BackColor = System.Drawing.Color.White;
+            this.pnlMain.Controls.Add(this.lblTitle);
+            this.pnlMain.Controls.Add(this.lblSubtitle);
+            this.pnlMain.Controls.Add(this.lblUsername);
+            this.pnlMain.Controls.Add(this.txtUsername);
+            this.pnlMain.Controls.Add(this.lblPassword);
+            this.pnlMain.Controls.Add(this.txtPassword);
+            this.pnlMain.Controls.Add(this.btnLogin);
+            this.pnlMain.Controls.Add(this.lnkRegister);
+            this.pnlMain.Controls.Add(this.lblError);
+            this.pnlMain.Location = new System.Drawing.Point(150, 50);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(380, 400);
+            this.pnlMain.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(33, 150, 243);
+            this.lblTitle.Location = new System.Drawing.Point(20, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(340, 45);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "MarkTogether";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSubtitle
+            // 
+            this.lblSubtitle.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblSubtitle.ForeColor = System.Drawing.Color.Gray;
+            this.lblSubtitle.Location = new System.Drawing.Point(20, 65);
+            this.lblSubtitle.Name = "lblSubtitle";
+            this.lblSubtitle.Size = new System.Drawing.Size(340, 25);
+            this.lblSubtitle.TabIndex = 1;
+            this.lblSubtitle.Text = "Đăng nhập để tiếp tục";
+            this.lblSubtitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblUsername.Location = new System.Drawing.Point(30, 115);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(77, 19);
+            this.lblUsername.TabIndex = 2;
+            this.lblUsername.Text = "Username:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.txtUsername.Location = new System.Drawing.Point(33, 140);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(310, 27);
+            this.txtUsername.TabIndex = 3;
+            this.txtUsername.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtUsername_KeyDown);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblPassword.Location = new System.Drawing.Point(30, 185);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(73, 19);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.txtPassword.Location = new System.Drawing.Point(33, 210);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(310, 27);
+            this.txtPassword.TabIndex = 5;
+            this.txtPassword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPassword_KeyDown);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(33, 150, 243);
+            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(33, 270);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(310, 40);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lnkRegister
+            // 
+            this.lnkRegister.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lnkRegister.Location = new System.Drawing.Point(30, 330);
+            this.lnkRegister.Name = "lnkRegister";
+            this.lnkRegister.Size = new System.Drawing.Size(316, 23);
+            this.lnkRegister.TabIndex = 7;
+            this.lnkRegister.TabStop = true;
+            this.lnkRegister.Text = "Chưa có tài khoản? Đăng ký";
+            this.lnkRegister.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lnkRegister.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkRegister_LinkClicked);
+            // 
+            // lblError
+            // 
+            this.lblError.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(30, 360);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(316, 30);
+            this.lblError.TabIndex = 8;
+            this.lblError.Text = "";
+            this.lblError.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblError.Visible = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(240, 240, 245);
+            this.ClientSize = new System.Drawing.Size(680, 500);
+            this.Controls.Add(this.pnlMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MarkTogether - Đăng nhập";
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblSubtitle;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.LinkLabel lnkRegister;
+        private System.Windows.Forms.Label lblError;
+    }
+}
---
+++ MarkTogether.Client/LoginForm.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Windows.Forms;
+using MarkTogether.Client.Network;
+using MarkTogether.Shared;
+
+namespace MarkTogether.Client
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text;
+
+            // Validate
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                lblError.Text = "Vui lòng nhập đầy đủ thông tin!";
+                lblError.Visible = true;
+                return;
+            }
+
+            try
+            {
+                btnLogin.Enabled = false;
+                btnLogin.Text = "Đang đăng nhập...";
+                lblError.Visible = false;
+
+                // Kết nối server (nếu chưa)
+                SocketClient.Instance.Connect("localhost", 5000);
+
+                // Gửi yêu cầu đăng nhập
+                AuthResponse result = SocketClient.Instance.Login(username, password);
+
+                if (result.Success)
+                {
+                    // Thành công → mở MainForm
+                    MessageBox.Show($"Chào mừng {result.Username}!",
+                        "Đăng nhập thành công",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+
+                    this.Hide();
+                    var mainForm = new Form1();
+                    mainForm.Text = $"MarkTogether - {result.Username}";
+                    mainForm.FormClosed += (s, args) =>
+                    {
+                        SocketClient.Instance.Disconnect();
+                        this.Close();
+                    };
+                    mainForm.Show();
+                }
+                else
+                {
+                    // Thất bại → hiện lỗi
+                    lblError.Text = result.Message;
+                    lblError.Visible = true;
+                    SocketClient.Instance.Disconnect();
+                }
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = "Không thể kết nối đến server!";
+                lblError.Visible = true;
+                MessageBox.Show($"Chi tiết lỗi: {ex.Message}",
+                    "Lỗi kết nối",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            finally
+            {
+                btnLogin.Enabled = true;
+                btnLogin.Text = "Đăng nhập";
+            }
+        }
+
+        private void lnkRegister_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Mở form đăng ký
+            var registerForm = new RegisterForm();
+            registerForm.ShowDialog(this);
+        }
+
+        // Cho phép nhấn Enter để đăng nhập
+        private void txtPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnLogin_Click(sender, e);
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void txtUsername_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                txtPassword.Focus();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+    }
+}
